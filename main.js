334581e545790e46a581ab7d1e10d6898d17e83c
JGO68 <48743389+JGO68@users.noreply.github.com>
2026-09-02 11:43:30 -0400

    fix: make French homepage canonical
    Closes #2

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// Language detection and redirection
-const userLang = navigator.language || navigator.userLanguage;
-if (!window.location.href.includes('index') && !window.location.href.includes('index-en') && !window.location.href.includes('index-es')) {
-    if (userLang.startsWith('fr')) {
-        window.location.href = 'index.html';
-    } else if (userLang.startsWith('en')) {
-        window.location.href = 'index-en.html';
-    } else if (userLang.startsWith('es')) {
-        window.location.href = 'index-es.html';
-    }
-}
-
 // Google Analytics
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
